5bcc03a5be18dc736aeec4ecd63bf22104baa157
null null <pkaoukis.2001@yahoo.gr>
2020-05-27 11:37:24 +0000

    ψορρεκτ

--- graphs.cpp
+++ graphs.cpp
@@ -1,88 +1,67 @@
-#include <iostream>
-#include <vector>
-#include <list>
-#include <set>
-using namespace std;
-
-
-class Graph {
- 
- private:
- int V;    
-list<int> *adj;    
-
- public:
-
- Graph(int V): V(V)
- {
-   adj = new list<int>[V];
- }
-
- ~Graph()
- {
-  delete [] adj;
- }
-
-void addEdge(int u, int v)
- {
-  adj[u].push_back(v);
- }
-
- bool cycle(vector<int> &path) const
- {
-   
- }
+#include <iostream>  
+#include <vector>  
+#include <list>  
+  
+#define nullptr NULL  
+  
+using namespace std;  
+  
+class Graph {  
+public:  
+    Graph(int V) {  
+        this->V = V;  
+        adj = new list<int>[V];  
+    }  
+  
+    ~Graph() { delete [] adj; }  
+  
+    void addEdge(int u, int v) {  
+        adj[u].push_back(v);  
+    }  
+  
+    bool cycle(vector<int> &path) const {  
+        int stack[V];  
+        bool visited[V];  
+        for(int i = 0; i < V; i++) {  
+          visited[i] = false;  
+          stack[i] = -1;  
+        }  
+        int start = 0;  
+        for (int i = 0; i < V; i++) {  
+          int j = 0;  
+          if (help (i, visited, stack, j, start)) {  
+            for (int k = start; k < j; k++)  
+              path.push_back(stack[k]);  
+            return 1;  
+          }  
+        }  
+        return 0;  
+    }  
+  
+protected:  
+    int V;  
+    list<int> *adj;  
+    bool help(int v, bool vis[], int st[], int &i, int &strt) const {  
+        if (!vis[v]) {  
+            vis[v] = true;  
+            st[i] = v;  
+            i++;  
+        }  
+        list<int>::iterator j;  
+        for(j = adj[v].begin(); j != adj[v].end(); ++j) {  
+            if(!vis[*j] && help(*j, vis, st, i, strt))  
+                return true;  
+            else {  
+                for (int k = 0; k < i; k++) {  
+                    if (st[k] == *j) {  
+                    strt = k;  
+                    return true;  
+                    }  
+                }  
+            }  
+        }  
+        st[v] = -1;  
+        i--;  
+        return false;  
+      }  
 };
-
-
-bool Graph::isCyclicUtil(int v, bool visited[], bool *recStack) 
-{ 
-    if(visited[v] == false) 
-    { 
-        // Mark the current node as visited and part of recursion stack 
-        visited[v] = true; 
-        recStack[v] = true; 
-  
-        // Recur for all the vertices adjacent to this vertex 
-        list<int>::iterator i; 
-        for(i = adj[v].begin(); i != adj[v].end(); ++i) 
-        { 
-            if ( !visited[*i] && isCyclicUtil(*i, visited, recStack) ) 
-                return true; 
-            else if (recStack[*i]) 
-                return true; 
-        } 
-  
-    } 
-    recStack[v] = false;  // remove the vertex from recursion stack 
-    return false; 
-} 
-  
-// Returns true if the graph contains a cycle, else false. 
-// This function is a variation of DFS() in https://www.geeksforgeeks.org/archives/18212 
-bool Graph::isCyclic() 
-{ 
-    // Mark all the vertices as not visited and not part of recursion 
-    // stack 
-    bool *visited = new bool[V]; 
-    bool *recStack = new bool[V]; 
-    for(int i = 0; i < V; i++) 
-    { 
-        visited[i] = false; 
-        recStack[i] = false; 
-    } 
-  
-    // Call the recursive helper function to detect cycle in different 
-    // DFS trees 
-    for(int i = 0; i < V; i++) 
-        if (isCyclicUtil(i, visited, recStack)) 
-            return true; 
-  
-    return false; 
-} 
-
-
-
-
-
-
